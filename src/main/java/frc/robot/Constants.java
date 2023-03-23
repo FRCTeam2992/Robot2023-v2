45f5f425ebcdb35c5c1d2d2b26b17f1e184c6b83
msonnier <mike@sonnier.name>
2023-03-23 15:11:04 -0500

    [BalanceRobot2] Add timers, use constants file, x wheels at end

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -87,7 +87,11 @@ public final class Constants {
     public static final double gyroRollOffset = -1.9; // degrees -- its robot pitch but navx roll
     public static final double pitchTolerance = 2.0; // degrees -- level if Abs() less than this
     public static final double pitchDeltaTolerance = 0.08; // degrees/20ms robot cycle
-    public static final double balanceMoveSpeed = 1.0; // m/sec -- how fast to crawl for final balance
+    public static final double balanceMoveSpeed = 0.75; // m/sec -- max speed to crawl for final balance
+    public static final double balanceWaitTimer = 2.0; // How long to wait before declaring balanced
+    public static final double balanceP = 0.05;
+    public static final double balanceI = 0.0;
+    public static final double balanceD = 0.006;
 
     // Drive Rotation P
     public static final double driveRotationP = .007;
--- src/main/java/frc/robot/commands/BalanceRobot2.java
+++ src/main/java/frc/robot/commands/BalanceRobot2.java
@@ -8,9 +8,12 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.filter.LinearFilter;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.Constants;
+import frc.robot.Robot;
 import frc.robot.subsystems.Drivetrain;
 
 public class BalanceRobot2 extends CommandBase {
@@ -20,8 +23,8 @@ public class BalanceRobot2 extends CommandBase {
     private double currentPitch;
     private double currentPitchDelta;
     private PIDController pitchPID;
-    private boolean moving = false;
     private boolean reached = false;
+    private Timer doneTimer;
 
     /** Creates a new BalanceRobot. */
     public BalanceRobot2(Drivetrain driveTrain) {
@@ -29,8 +32,10 @@ public class BalanceRobot2 extends CommandBase {
         mDrivetrain = driveTrain;
         addRequirements(mDrivetrain);
         lowPass = LinearFilter.movingAverage(3);
-        pitchPID = new PIDController(.05, 0, 0.006);
-
+        pitchPID = new PIDController(Constants.DrivetrainConstants.balanceP,
+                Constants.DrivetrainConstants.balanceI,
+                Constants.DrivetrainConstants.balanceD);
+        doneTimer = new Timer();
     }
 
     // Called when the command is initially scheduled.
@@ -40,13 +45,14 @@ public class BalanceRobot2 extends CommandBase {
         lowPass.reset();
         mDrivetrain.setDriveNeutralMode(NeutralMode.Brake);
         reached = false;
-
+        doneTimer.reset();
+        doneTimer.stop();
     }
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
     public void execute() {
-        double speed = 0.0;
+        double speed;
 
         priorPitch = currentPitch;
         currentPitch = lowPass.calculate(mDrivetrain.getRobotPitch());
@@ -57,12 +63,20 @@ public class BalanceRobot2 extends CommandBase {
         SmartDashboard.putBoolean("Robot Balance Reached", reached);
 
         speed = pitchPID.calculate(currentPitch);
-        if ((Math.abs(currentPitch) < 2.0) || reached) {
+        if ((Math.abs(currentPitch) < Constants.DrivetrainConstants.pitchTolerance) || reached) {
             reached = true;
             speed *= 0.3;
         }
-        speed = Math.max(-0.75, speed);
-        speed = Math.min(0.75, speed);
+
+        if (Math.abs(currentPitch) < Constants.DrivetrainConstants.pitchDeltaTolerance) {
+            // We are within the balanced range -- start timer
+            doneTimer.start();
+        } else {
+            // Outside the range so reset the timer
+            doneTimer.restart();
+        }
+        speed = Math.max(-Constants.DrivetrainConstants.balanceMoveSpeed, speed);
+        speed = Math.min(Constants.DrivetrainConstants.balanceMoveSpeed, speed);
 
         if (speed > 0.0) {
             mDrivetrain.moveRobotFrontBack(true, -speed);
@@ -75,11 +89,20 @@ public class BalanceRobot2 extends CommandBase {
     // Called once the command ends or is interrupted.
     @Override
     public void end(boolean interrupted) {
+        mDrivetrain.frontLeftModule.setTurnAngle(45.0);
+        mDrivetrain.frontLeftModule.setDriveSpeed(0.0);
+        mDrivetrain.frontRightModule.setTurnAngle(-45.0);
+        mDrivetrain.frontRightModule.setDriveSpeed(0.0);
+        mDrivetrain.rearLeftModule.setTurnAngle(-45.0);
+        mDrivetrain.rearLeftModule.setDriveSpeed(0.0);
+        mDrivetrain.rearRightModule.setTurnAngle(45.0);
+        mDrivetrain.rearRightModule.setDriveSpeed(0.0);
     }
 
     // Returns true when the command should end.
     @Override
     public boolean isFinished() {
-        return false;
+        return ((doneTimer.get() > Constants.DrivetrainConstants.balanceWaitTimer) ||
+                (Robot.balanceTimer.get() > 14.0));
     }
 }
