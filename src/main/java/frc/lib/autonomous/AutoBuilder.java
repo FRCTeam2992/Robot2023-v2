2ec261e7cc4a6fd33da42669995fea8e7d79a67d
msonnier <mike@sonnier.name>
2023-03-24 14:10:50 -0500

    cleanup white apce and remove unused import

--- src/main/java/frc/lib/autonomous/AutoBuilder.java
+++ src/main/java/frc/lib/autonomous/AutoBuilder.java
@@ -6,7 +6,6 @@ package frc.lib.autonomous;
 
 import java.util.HashMap;
 import com.pathplanner.lib.PathPlannerTrajectory;
-import com.pathplanner.lib.PathPlannerTrajectory.EventMarker;
 import com.pathplanner.lib.commands.FollowPathWithEvents;
 
 import edu.wpi.first.math.geometry.Pose2d;
