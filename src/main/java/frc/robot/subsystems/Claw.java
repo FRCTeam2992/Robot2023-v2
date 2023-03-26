ab1201a4c4859a5fbade6ee8b24ab2f36cef4250
msonnier <mike@sonnier.name>
2023-03-25 19:52:26 -0500

    [HoldClaw] Fix to use PID

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -33,10 +33,7 @@ public class Claw extends SubsystemBase {
                 true, 5.0,
                 25.0, 0.1));
 
-        clawMotor.config_kP(0, Constants.ClawConstants.PIDConstants.P);
-        clawMotor.config_kI(0, Constants.ClawConstants.PIDConstants.I);
-        clawMotor.config_kD(0, Constants.ClawConstants.PIDConstants.D);
-        clawMotor.config_kF(0, Constants.ClawConstants.PIDConstants.FF);
+        setPIDConstants(clawMotor);
     }
 
     @Override
@@ -54,6 +51,7 @@ public class Claw extends SubsystemBase {
     }
 
     public void setClawSpeed(double speed) {
+        holdPositionRecorded = false;
         clawMotor.set(TalonFXControlMode.PercentOutput, speed);
     }
 
@@ -70,19 +68,25 @@ public class Claw extends SubsystemBase {
     }
 
     public void holdClaw() {
-        clawMotor.set(TalonFXControlMode.PercentOutput, 0.1);
-        // if (!holdPositionRecorded) {
-        //     // We haven't recorded where we are yet, so get it
-        //     holdPosition = getClawMotorPosition();
-        //     holdPositionRecorded = true;
-        //     clawMotor.set(TalonFXControlMode.PercentOutput, 0.0);
-        // } else {
-        //     clawMotor.set(TalonFXControlMode.Position, holdPosition);
-        // }
+        if (!holdPositionRecorded) {
+            // We haven't recorded where we are yet, so get it
+            holdPosition = getClawMotorPosition();
+            holdPositionRecorded = true;
+            clawMotor.set(TalonFXControlMode.PercentOutput, 0.0);
+        } else {
+            clawMotor.set(TalonFXControlMode.Position, holdPosition);
+        }
 
     }
 
+    private void setPIDConstants(TalonFX motor) {
+        motor.config_kP(0, Constants.ClawConstants.PIDConstants.P);
+        motor.config_kI(0, Constants.ClawConstants.PIDConstants.I);
+        motor.config_kD(0, Constants.ClawConstants.PIDConstants.D);
+        motor.config_kF(0, Constants.ClawConstants.PIDConstants.FF);
+    }
     public void onDisable() {
         clawMotor.set(ControlMode.PercentOutput, 0.0);
+        holdPositionRecorded = false;
     }
 }
