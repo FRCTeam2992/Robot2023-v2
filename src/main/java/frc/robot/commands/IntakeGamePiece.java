ab4540909985e9dac5a61fe7a60edf833df40fe6
Ryan Harvey <ryan.b.harvey@gmail.com>
2023-03-29 19:56:24 -0500

    Cleanup bad merge

--- src/main/java/frc/robot/commands/IntakeGamePiece.java
+++ src/main/java/frc/robot/commands/IntakeGamePiece.java
@@ -7,54 +7,31 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.ClawConstants;
+import frc.robot.Constants;
 import frc.robot.RobotState;
 import frc.robot.subsystems.Claw;
 
 public class IntakeGamePiece extends CommandBase {
-  private Claw mClaw;
-  private RobotState mRobotState;
+    private Claw mClaw;
+    private RobotState mRobotState;
 
-  private int cyclesAfterBeamBreak;
-  private double speed;
+    private int cyclesAfterBeamBreak;
+    private double speed;
 
-  /** Creates a new IntakeGamePiece. */
-  public IntakeGamePiece(Claw claw, RobotState robotState) {
-    mClaw = claw;
-    mRobotState = robotState;
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(mClaw);
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    cyclesAfterBeamBreak = 0;
-    speed = 0.0;
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    switch (mRobotState.intakeMode) {
-      case Cube:
-        speed = ClawConstants.Intake.Speed.cube;
-        break;
-      case Cone:
-      case Unknown:
-      default:
-        speed = ClawConstants.Intake.Speed.cone;
+    /** Creates a new IntakeGamePiece. */
+    public IntakeGamePiece(Claw claw, RobotState robotState) {
+        mClaw = claw;
+        mRobotState = robotState;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(mClaw);
     }
-    mClaw.setClawSpeed(speed);
-    if (mClaw.getBeamBreakTriggered()) {
-      cyclesAfterBeamBreak++;
-    } else {
-      cyclesAfterBeamBreak = 0;
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        cyclesAfterBeamBreak = 0;
+        speed = 0.0;
     }
-<<<<<<< Updated upstream
-    // Troubleshooting only dashboard
-    // SmartDashboard.putNumber("Beam Break Cycles", cyclesAfterBeamBreak);
-  }
-=======
 
     // Called every time the scheduler runs while the command is scheduled.
     @Override
@@ -79,24 +56,23 @@ public class IntakeGamePiece extends CommandBase {
             SmartDashboard.putNumber("Beam Break Cycles", cyclesAfterBeamBreak);
         }
     }
->>>>>>> Stashed changes
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-      mClaw.setClawSpeed(0.0);
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    switch (mRobotState.intakeMode) {
-      case Cube:
-        return cyclesAfterBeamBreak >= ClawConstants.Intake.DelayCyclesAfterBeamBreak.cube;
-      case Cone:
-      case Unknown:
-      default:
-        return cyclesAfterBeamBreak >= ClawConstants.Intake.DelayCyclesAfterBeamBreak.cone;
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        mClaw.setClawSpeed(0.0);
     }
-  }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        switch (mRobotState.intakeMode) {
+            case Cube:
+                return cyclesAfterBeamBreak >= ClawConstants.Intake.DelayCyclesAfterBeamBreak.cube;
+            case Cone:
+            case Unknown:
+            default:
+                return cyclesAfterBeamBreak >= ClawConstants.Intake.DelayCyclesAfterBeamBreak.cone;
+        }
+    }
 }
