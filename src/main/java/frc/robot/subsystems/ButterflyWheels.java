8510d0d88b010498927dd5a17e779f0e0c7ff245
Reid-Jourdan <71857615+Reid-Jourdan@users.noreply.github.com>
2023-04-13 15:09:41 -0500

    add beam breaks and display
    tested and working

--- src/main/java/frc/robot/subsystems/ButterflyWheels.java
+++ src/main/java/frc/robot/subsystems/ButterflyWheels.java
@@ -5,14 +5,21 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ButterflyWheels extends SubsystemBase {
 
     private Solenoid butterflyWheelsSolenoid;
+
+    private DigitalInput butterflyBeamBreakFront;
+    private DigitalInput butterflyBeamBreakBack;
+
+    private int dashboardCounter = 0;
 
     public enum ButterflyWheelsState {
         Undeployed(false),
@@ -29,11 +36,25 @@ public class ButterflyWheels extends SubsystemBase {
     public ButterflyWheels() {
         butterflyWheelsSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM,
                 Constants.ButterflyWheelsConstants.DeviceIDs.butterflyWheelsSolenoid);
+
+        butterflyBeamBreakFront = new DigitalInput(
+                Constants.ButterflyWheelsConstants.DeviceIDs.butterflyBeamBreakFront);
+        butterflyBeamBreakBack = new DigitalInput(
+                Constants.ButterflyWheelsConstants.DeviceIDs.butterflyBeamBreakBack);
     }
 
     @Override
     public void periodic() {
         // This method will be called once per scheduler run
+        if (dashboardCounter++ > 5) {
+            SmartDashboard.putBoolean("Butterfly Wheels", (!getBackBeamBreak() && !getFrontBeamBreak()));
+            if (Constants.debugDashboard) {
+                SmartDashboard.putBoolean("Butterfly Wheels Front", (!getFrontBeamBreak()));
+                SmartDashboard.putBoolean("Butterfly Wheels Back", (!getBackBeamBreak()));
+            }
+            dashboardCounter = 0;
+        }
+
     }
 
     public void setButterflyWheelsState(ButterflyWheelsState state) {
@@ -42,6 +63,14 @@ public class ButterflyWheels extends SubsystemBase {
 
     public boolean getButterflyWheelsSolenoidState() {
         return butterflyWheelsSolenoid.get();
+    }
+
+    public boolean getFrontBeamBreak() {
+        return butterflyBeamBreakFront.get();
+    }
+
+    public boolean getBackBeamBreak() {
+        return butterflyBeamBreakBack.get();
     }
 
     public void onDisable() {
