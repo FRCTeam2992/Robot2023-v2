98ee01e4dd65bd992db66280f81e29d8643690e0
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2023-04-19 14:39:49 -0500

    rezero swerves

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -58,9 +58,9 @@ public final class Constants {
         // Analog Encoder Offsets (Degrees) - Opposite of Raw Reading - Bevel Gear to
         // Right
         public static final double frontLeftOffset = -174.3;
-        public static final double frontRightOffset = 90.8;
-        public static final double rearLeftOffset = 170.6;
-        public static final double rearRightOffset = 31.0;
+        public static final double frontRightOffset = 95.0; //90.8
+        public static final double rearLeftOffset = 180.6;//170.6
+        public static final double rearRightOffset = 28.3;//31.0
 
         public static class PIDConstants {
             // Swerve Drive PID (Velocity Control)
