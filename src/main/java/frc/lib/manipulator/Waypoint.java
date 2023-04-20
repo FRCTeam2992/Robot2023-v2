2582ece6ddca6fc3915757a56fbf4568b8b49e44
Ryan B. Harvey <ryan.b.harvey@gmail.com>
2023-04-20 07:11:26 -0500

    autos tuning

--- src/main/java/frc/lib/manipulator/Waypoint.java
+++ src/main/java/frc/lib/manipulator/Waypoint.java
@@ -47,7 +47,7 @@ public class Waypoint extends Point2D.Double {
         Mid_Cube(-0.8, 1.0),
         Hybrid(-0.35, 1.0),
         Max_Throw_Cube(-1.0, 1.0),
-        Rear_Low_Cube(-0.4, 1.0);
+        Rear_Low_Cube(-0.25, 1.0);
 
         public double speed; // How fast to spin claw
         public double time; // How long to spin claw
